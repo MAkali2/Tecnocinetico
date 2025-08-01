ac0868599f4e95e53af8a3cf02f22e9633b7a681
MAkali2 <102842519+MAkali2@users.noreply.github.com>
2025-08-01 17:48:27 -0500

    correciones del codigo

--- src/main/java/com/bryan26/tecnocinetico/core/registry/ModBlockEntities.java
+++ src/main/java/com/bryan26/tecnocinetico/core/registry/ModBlockEntities.java
@@ -1,7 +1,7 @@
 package com.bryan26.tecnocinetico.core.registry;
 
-import com.bryan26.tecnocinetico.Tecnocinetico;
-import com.bryan26.tecnocinetico.content.blockentity.PrensaElectricaBlockEntity;
+import com.bryan26.tecnocinetico.content.block.machines.PrensaElectricaBlockEntity;
+
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -9,7 +9,7 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockEntities {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES =
-        DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, Tecnocinetico.MOD_ID);
+        DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, "tecnocinetico");
 
     public static final RegistryObject<BlockEntityType<PrensaElectricaBlockEntity>> PRENSA_ELECTRICA =
         BLOCK_ENTITIES.register("prensa_electrica",
