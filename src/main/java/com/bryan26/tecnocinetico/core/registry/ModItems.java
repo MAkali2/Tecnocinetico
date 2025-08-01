2acca2083bf6f10143cd4be3593d18bc13457652
MAkali2 <102842519+MAkali2@users.noreply.github.com>
2025-08-01 17:48:01 -0500

    correciones del codigo

--- src/main/java/com/bryan26/tecnocinetico/core/registry/ModItems.java
+++ src/main/java/com/bryan26/tecnocinetico/core/registry/ModItems.java
@@ -1,5 +1,13 @@
 package com.bryan26.tecnocinetico.core.registry;
 
-public class ModItems {
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "tecnocinetico");
+
+    public static final RegistryObject<Item> IRON_PLATE = ITEMS.register("iron_plate",
+        () -> new Item(new Item.Properties()));
 }
