cbc51cdf70712e3122a8857bc35bb13ebcb594d1
MAkali2 <102842519+MAkali2@users.noreply.github.com>
2025-08-01 17:48:18 -0500

    correciones del codigo

--- src/main/java/com/bryan26/tecnocinetico/core/registry/ModBlocks.java
+++ src/main/java/com/bryan26/tecnocinetico/core/registry/ModBlocks.java
@@ -1,25 +1,16 @@
 package com.bryan26.tecnocinetico.core.registry;
 
-import com.bryan26.tecnocinetico.Tecnocinetico;
 import com.bryan26.tecnocinetico.content.block.machines.PrensaElectricaBlock;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
+
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlocks {
-    public static final DeferredRegister<Block> BLOCKS =
-        DeferredRegister.create(ForgeRegistries.BLOCKS, Tecnocinetico.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, "tecnocinetico");
 
-    public static final DeferredRegister<Item> ITEMS =
-        DeferredRegister.create(ForgeRegistries.ITEMS, Tecnocinetico.MOD_ID);
-
-    public static final RegistryObject<Block> PRENSA_ELECTRICA_BLOCK =
-        BLOCKS.register("prensa_electrica", PrensaElectricaBlock::new);
-
-    public static final RegistryObject<Item> PRENSA_ELECTRICA_ITEM =
-        ITEMS.register("prensa_electrica",
-            () -> new BlockItem(PRENSA_ELECTRICA_BLOCK.get(), new Item.Properties()));
+    public static final RegistryObject<Block> PRENSA_ELECTRICA_BLOCK = BLOCKS.register("prensa_electrica",
+        () -> new PrensaElectricaBlock(Block.Properties.of(Material.METAL).strength(3.5F)));
 }
